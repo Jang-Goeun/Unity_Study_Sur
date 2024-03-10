8016911d8633b0f1940aa07e1fadd4eef44b2686
goeun441 <20200298@dongduk.ac.kr>
2024-03-10 12:22:49 +0900

    뱀서라이크 1, 2, 3

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextvec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        //1. 힘을 준다.
+        //rigid.AddForce(inputVec);
+
+        //2. 속도 제어
+        //rigid.velocity = inputVec;
+
+        //3. 위치 이동
+        rigid.MovePosition(rigid.position + nextvec);
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+        if(inputVec.x != 0) {
+            spriter.flipX = (inputVec.x < 0);
+        }
+    }
+}
